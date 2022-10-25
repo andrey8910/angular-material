5ee4795b1c05d1e0958bbc2a22c027bb40948758
ivanov6444 <a.ivanov6444@gmail.com>
2022-10-25 18:03:00 +0300

    add mat-input-module & mat-icon-module & mat-table-module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from "@angular/material/button";
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatRadioModule } from '@angular/material/radio';
-
-
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
     MatTabsModule,
-    MatRadioModule
+    MatRadioModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule
+
   ],
-  exports: [MatButtonModule,MatTabsModule,MatRadioModule]
+  exports: [
+    MatButtonModule,
+    MatTabsModule,
+    MatRadioModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule
+  ]
 })
 export class MaterialModule { }
