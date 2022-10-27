641a7eb2c2bfbcc4af40faa0e7df8c435f8119c0
ivanov6444 <a.ivanov6444@gmail.com>
2022-10-27 14:45:29 +0300

    mat-dialog/two dialogs in one component

--- src/app/dialog-second/first-companion-dialog-second/first-companion-dialog-second.component.ts
+++ src/app/dialog-second/first-companion-dialog-second/first-companion-dialog-second.component.ts
@@ -35,4 +35,6 @@ export class FirstCompanionDialogSecondComponent implements OnInit {
       console.log(result)
     });
   }
+
+
 }
--- src/app/dialog/first-companion/first-companion.component.ts
+++ src/app/dialog/first-companion/first-companion.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import { SecondCompanionComponent } from '../second-companion/second-companion.component'
+import {
+  DialogSecondData,
+  SecondCompanionDialogSecondComponent
+} from "../../dialog-second/second-companion-dialog-second/second-companion-dialog-second.component";
 
 @Component({
   selector: 'app-first-companion',
@@ -8,26 +12,45 @@ import { SecondCompanionComponent } from '../second-companion/second-companion.c
   styleUrls: ['./first-companion.component.css']
 })
 export class FirstCompanionComponent implements OnInit {
-  name: string = '';
-  answerCompanion: string = '';
+  dialogFirstName: string = '';
+  answerCompanionDialogFirst: string = '';
+
+  answerCompanionDialogSecond: DialogSecondData = {
+    answerText : '',
+    answerColor : ''
+  };
 
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
-  openDialog(){
+  public openDialogFirst(){
     const dialogRef = this.dialog.open(SecondCompanionComponent, {
       width: '250px',
-      data: {name: this.name, answer: this.answerCompanion},
+      data: {name: this.dialogFirstName, answer: this.answerCompanionDialogFirst},
       disableClose: true,
       panelClass: 'modal-dialog'
 
     });
 
     dialogRef.afterClosed().subscribe(result => {
+      this.answerCompanionDialogFirst = result;
+      console.log(result)
+    });
+  }
 
-      this.answerCompanion = result;
+  public openDialogSecond(){
+    const dialogRef = this.dialog.open(SecondCompanionDialogSecondComponent, {
+      width: '250px',
+      data: {answerText : this.answerCompanionDialogSecond.answerText, answerColor: this.answerCompanionDialogSecond.answerColor},
+      disableClose: true,
+      panelClass: 'modal-dialog-second'
+
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.answerCompanionDialogSecond = result;
       console.log(result)
     });
   }
